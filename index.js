b884381f61cb777eac44d105533b2ac3cb36b95c
Tina Hang <mstinahang@gmail.com>
2020-02-23 00:09:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ class Scooter(year, color, model) {
   this.color = color;
   this.model = model;
 }
+
+class Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
